cb16d358fcc6a422416ba544aa77db3d4fb34143
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-03-29 16:03:10 -0700

    added code to make page talk and remove messages when clicked

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  //alert("jQuery is working!"); //for texgting jQuery
+  $("button#hello").click(function() {
+    $("ul#user").prepend("<li>Hello!</li>");
+    $("ul#webpage").prepend("<li>Why hello there!</li>");
+    $("li").click(function() {
+      //$(this).css("background-color","lime");
+      $(this).remove();
+    });
+  });
+  $("button#goodbye").click(function() {
+    $("ul#user").prepend("<li>Goodbye!</li>");
+    $("ul#webpage").prepend("<li>Goodbye, dear user!</li>");
+  });
+  $("button#stop").click(function() {
+    $("ul#user").prepend("<li>Stop copying me!</li>");
+    $("ul#webpage").prepend("<li>Pardon me. I meant no offense.</li>");
+  });
+
+});
